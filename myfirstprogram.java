e1556d6b970d3f8e7c7650d50066a2b850ec7523
suhanigupta23 <144280247+suhanigupta23@users.noreply.github.com>
2023-12-22 21:55:44 +0530

    Create hello world in java

---
+++ myfirstprogram.java
@@ -0,0 +1,9 @@
+public class myfirstprogram
+{
+    public static void main(String[] args) 
+    {
+        System.out.print("hello\n");
+        System.out.println("hello!this is using 2nd printing style");
+        System.out.print("hello");
+	}
+}
