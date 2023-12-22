6350c6c726a2c9b764b2df3afacc4283bf840d7f
suhanigupta23 <144280247+suhanigupta23@users.noreply.github.com>
2023-12-22 21:58:17 +0530

    Create printing stars

---
+++ printingstar.java
@@ -0,0 +1,12 @@
+public class Program
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.print("*\n**\n***\n****\n*****");
+	}
+}
