b2c30f5ff4941cabf0258b6e580459d8d2f395a0
suhanigupta23 <144280247+suhanigupta23@users.noreply.github.com>
2023-12-25 20:30:07 +0530

    even odd in java

---
+++ evenoddjava.java
@@ -0,0 +1,16 @@
+public class Program
+{
+    public static void main(String[] args) 
+    {
+		int x=11;
+        if (x%2==0)
+        {
+            System.out.println(x+" is even");
+        }
+        else
+        {
+            System.out.println(x+" is odd");
+        }
+        System.out.println("HARDWORK IS BETTER THAN SMART WORK.");
+	}
+}
