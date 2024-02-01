0cf714dcb487afce6477ffef3571d187668eff74
Suhani Gupta <144280247+suhanigupta23@users.noreply.github.com>
2024-02-01 19:32:57 +0530

    Permutation in java

---
+++ permutation.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Program
+{
+    public static void main(String[] args) 
+    {
+		Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int r=scn.nextInt();
+
+        int nfact=1;
+        for(int i=1;i<=n;i++)
+        {
+            nfact*=i;
+        }
+        int nmrfact=1;
+        for(int i=1;i<=n-r;i++)
+        {
+            nmrfact*=i;
+        }
+        int npr=nfact/nmrfact;
+        System.out.println(n+"P"+r+" = "+npr);
+	}
+}
