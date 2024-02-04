80314b601154bd47342869c4d133ab1f396af5c1
Suhani Gupta <144280247+suhanigupta23@users.noreply.github.com>
2024-02-04 18:20:33 +0530

    finding the differnece between the maximum and minimum value input

---
+++ span_finding_difference_between_maximum_and_minimum_value_input.java
@@ -0,0 +1,33 @@
+//FINDING THE DIFFERNECE BETWEEN THE MAXIMUM AND MINIMUM VALUES INPUT  (SPAN)
+
+
+import java.util.*;
+public class Program
+{
+    public static void main(String[] args) 
+    {
+        Scanner scn=new Scanner (System.in);
+        int n=scn.nextInt();
+        int []arr=new int [n];
+
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=scn.nextInt();
+        }
+        int max=arr[0];
+        int min=arr[0];
+        for(int i=1;i<arr.length;i++)
+        {
+            if(arr[i]>max)
+            {
+                max=arr[i];
+            }
+            if(arr[i]<min)
+            {
+                min=arr[i];
+            }
+        }
+        int span=(max-min);
+        System.out.println(span);
+	}
+}
