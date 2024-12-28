6fa6939ccbc48ecab3cf94ec0a17ccf7479b6d45
Suhani Gupta <144280247+suhanigupta23@users.noreply.github.com>
2024-12-28 17:37:25 +0530

    Create celebrity.java

---
+++ celebrity.java
@@ -0,0 +1,11 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+// CELEBRITY IS ONE WHO : IS KNOWN BY ALL , BUT DO NOT KNOW ANYONE 
+//THIS IS CELEBRITY PROBLEM 
+// A SQUARE MATRIX MADE , ON LEFT TO RIGHT 0 TO X NUMBER WRITTEN , ON TOP TO BOTTOM ALSO 0 TO X NUMBER WRITTEN. 
+//NOW IF ROW NUMBER X KNOWS THE PEOPLE ON  X' NUMBER ON THE COLUMN THEN WE WRITE 1 , IF DO NOT KNOW THEN WE WRITE 0 .
+class Main {
+    public static void main(String[] args) {
+       
+    }
+}
